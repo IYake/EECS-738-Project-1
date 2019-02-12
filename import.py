9cdd8651e8cc320a8431f58e1ec5bb7ad4e31d07
Ian Yake <iangyake@gmail.com>
2019-02-12 16:06:48 -0600

    read in relevant columns from breast cancer data

--- import.py
+++ import.py
@@ -5,4 +5,11 @@ Created on Tue Feb 12 15:45:21 2019
 @author: Ian
 """
 
+import pandas as pd
+
+df = pd.read_csv('data/breast_cancer.csv')
+radiuses = df.radius_mean
+fractals = df.fractal_dimension_worst
+diagnoses = df.diagnosis
+ 
 
