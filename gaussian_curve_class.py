f03a02176f43fe15b5a0b7191549a95acec27024
Ian Yake <iangyake@gmail.com>
2019-02-13 09:49:00 -0600

    add multivariate normal dist to gaussian curve class

--- gaussian_curve_class.py
+++ gaussian_curve_class.py
@@ -1,15 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 12 17:59:21 2019
-
-@author: Ian
-"""
+from scipy.stats import multivariate_normal
 
 class gaussian_curve:
     def __init__(self, mean, cov_matrix, pi): #pi = gaussian weighting factor
         self.mean = mean
         self.cov_matrix = cov_matrix
         self.pi = pi
+        self.normal = multivariate_normal(mean,cov_matrix)
         
     def set_mean(self, value):
         self.mean = value
@@ -25,3 +21,6 @@ class gaussian_curve:
         self.pi = value
     def get_pi(self):
         return self.pi
+    
+    def update_normal(self):
+        self.normal = multivariate_normal(mean,cov_matrix)
--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 12 15:45:21 2019
-
-@author: Ian
-"""
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
