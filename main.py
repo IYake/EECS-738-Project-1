5f8c78ec1f5dcfc27b2f417b7604a90afa5a1f8a
Ian Yake <iangyake@gmail.com>
2019-02-13 09:20:00 -0600

    generalize plot and change X to concavity mean

--- main.py
+++ main.py
@@ -11,13 +11,16 @@ import numpy as np
 import gaussian_curve_class as gcc
 
 #read data
-df = pd.read_csv('data/breast_cancer.csv', usecols = ['diagnosis', 'radius_mean', 'fractal_dimension_worst'])
+Y = 'radius_mean'
+X = 'concavity_mean'
+
+df = pd.read_csv('data/breast_cancer.csv', usecols = ['diagnosis', Y, X])
 benign = df.loc[df['diagnosis'] == 'B']
 malignant = df.loc[df['diagnosis'] == 'M']
 
 #plot data
-ax = benign.plot(kind='scatter',x = 'fractal_dimension_worst', y ='radius_mean',color='red', label = 'malignant')
-malignant.plot(kind='scatter',x = 'fractal_dimension_worst', y ='radius_mean', color='blue', ax=ax, label='benign')
+ax = benign.plot(kind='scatter',x = X, y = Y,color='red', label = 'malignant')
+malignant.plot(kind='scatter',x = X, y = Y, color='blue', ax=ax, label='benign')
 
 
 #create random means and stdevs and gaussian weighting factor
@@ -32,22 +35,30 @@ cov_matrix2 = np.array([[0.02,0],[0,5]])
 pi1 = 1.0/numClasses
 pi2 = pi1
 
-gcc1 = gcc.gaussian_curve(mean1, stdev1, pi1)
-gcc2 = gcc.gaussian_curve(mean2, stdev2, pi1)
+gcc1 = gcc.gaussian_curve(mean1, cov_matrix1, pi1)
+gcc2 = gcc.gaussian_curve(mean2, cov_matrix2, pi1)
 
 #assign responsibility for each point to each gaussian curve
 #find new means, stdevs, and gaussian weighting factors
 
-###plot###
 
+
+
+
+
+
+
+
+###plot###
 #.plot(x='x', y='y', ax=ax, style='gx', label='mean2')
 #mean1.plot(x='x', y='y', ax=ax, style='gx', label='mean1')
-ax.set_xlabel("fractal_dimension_worst")
-ax.set_ylabel("radius_mean")
+ax.set_xlabel(X)
+ax.set_ylabel(Y)
 #continue until likelihood difference between iterations passes tolerance level
 gcc = gcc.gaussian_curve(mean1,20,30)
 
 plt.plot(mean1[0],mean1[1],'gx')
+plt.plot(mean2[0],mean2[1],'gx')
 #plt.plot([1,2,3,4], [1,4,9,16], 'ro')
 #plt.axis([0, 6, 0, 20])
 #plt.show()
