0c2a1260fa046d57d7bf498079cdbed176fdd41a
Ian Yake <iangyake@gmail.com>
2019-02-18 23:16:14 -0600

    normalize second dataset

--- main2.py
+++ main2.py
@@ -1,11 +1,10 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 import gaussian_curve_class as gcc
 
 
 #read data
-Y_label = 'pH'
+Y_label = 'sulphates'
 X_label = 'volatile acidity'
 wine = pd.read_csv('data/winequality-red.csv', usecols = ['quality_chr', Y_label, X_label])
 class1 = 'G'
@@ -15,6 +14,8 @@ good = wine.loc[wine[class_feature] == class1]
 bad  = wine.loc[wine[class_feature] == class2]
 frames = [good, bad]
 df = pd.concat(frames, ignore_index = True)
+df[X_label] = (df[X_label]-df[X_label].mean()) / (df[X_label].max()-df[X_label].min())
+df[Y_label] = (df[Y_label]-df[Y_label].mean()) / (df[Y_label].max()-df[Y_label].min())
 print(df.shape)
 
 #create random means and sigmas and gaussian weighting factors
@@ -22,8 +23,8 @@ numClasses = 2
 xMax, xMin, yMax, yMin = df.loc[:,X_label].max(), df.loc[:,X_label].min(), df.loc[:,Y_label].max(), df.loc[:,Y_label].min()
 mu_1 = np.array([(xMax-xMin)*3/4+xMin,(yMax-yMin)*1/4+yMin])
 mu_2 = np.array([(xMax-xMin)*1/4+xMin,(yMax-yMin)*3/4+yMin])
-sigma1 = np.array([[(xMax-xMin)/128,0],[0,(yMax-yMin)/8]])
-sigma2 = np.array([[(xMax-xMin)/128,0],[0,(yMax-yMin)/8]])
+sigma1 = np.array([[(xMax-xMin)/(2**6),0],[0,(yMax-yMin)/(2**6)]])
+sigma2 = np.array([[(xMax-xMin)/(2**6),0],[0,(yMax-yMin)/(2**6)]])
 pi1 = 1.0/numClasses
 pi2 = 1.0/numClasses
 curve1 = gcc.gaussian_curve(mu_1, sigma1, pi1)
@@ -77,7 +78,6 @@ for i in range(steps):
     gcc.plot_curves(i,df,X_label,Y_label,class_feature,class1,class2,curve1,curve2)
     gcc.iterate(curve1, curve2, X, Y)
     curr_log_likelihood = gcc.log_likelihood(curve1,curve2)
-    print(curr_log_likelihood)
     if (abs(curr_log_likelihood - previous_log_likelihood) < tolerance):
         gcc.plot_curves(i,df,X_label,Y_label,class_feature,class1,class2,curve1,curve2)
         break
