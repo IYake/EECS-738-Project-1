bf99c9dfd7d011bf7e5f4fa485bdce82d46fd818
Jules Garrett <jrgarrett3222@gmail.com>
2019-02-18 19:53:49 -0600

    add second data source and new second main file

--- main2.py
+++ main2.py
@@ -9,15 +9,18 @@ from mpl_toolkits.mplot3d import Axes3D
 import inspect
 
 #read data
-Y_label = 'radius_mean'
-X_label = 'concavity_mean'
-df = pd.read_csv('data/breast_cancer.csv', usecols = ['diagnosis', Y_label, X_label])
-benign = df.loc[df['diagnosis'] == 'B']
-malignant = df.loc[df['diagnosis'] == 'M']
+Y_label = 'pH'
+X_label = 'volatile acidity'
+df = pd.read_csv('data/winequality-red.csv', usecols = ['quality', Y_label, X_label])
+
+good = df.loc[df['quality'] >= 7 ]
+bad = df.loc[df['quality'] <= 4 ]
+frames = [good, bad]
+wine = pd.concat(frames, ignore_index = True)
 
 #create random means and sigmas and gaussian weighting factors
 numClasses = 2
-xMax, xMin, yMax, yMin = df.loc[:,X_label].max(), df.loc[:,X_label].min(), df.loc[:,Y_label].max(), df.loc[:,Y_label].min()
+xMax, xMin, yMax, yMin = wine.loc[:,X_label].max(), wine.loc[:,X_label].min(), wine.loc[:,Y_label].max(), wine.loc[:,Y_label].min()
 mu_1 = np.array([(xMax-xMin)*3/4+xMin,(yMax-yMin)*1/4+yMin])
 mu_2 = np.array([(xMax-xMin)*1/4+xMin,(yMax-yMin)*3/4+yMin])
 sigma1 = np.array([[(xMax-xMin)/32,0],[0,(yMax-yMin)/2]])
@@ -30,21 +33,21 @@ curve2 = gcc.gaussian_curve(mu_2, sigma2, pi1)
 
 
 #reshape X column and Y column so they can be processed for Z
-numPoints = df.shape[0]
+numPoints = wine.shape[0]
 if (numPoints % 2 != 0):
-    X = df.loc[:numPoints-2,X_label].values
-    Y = df.loc[:numPoints-2,Y_label].values
+    X = wine.loc[:numPoints-2,X_label].values
+    Y = wine.loc[:numPoints-2,Y_label].values
 else:
-    X = df.loc[:numPoints,X_label].values
-    Y = df.loc[:numPoints,Y_label].values
+    X = wine.loc[:numPoints,X_label].values
+    Y = wine.loc[:numPoints,Y_label].values
 X_arrs = np.array_split(X,2)
 Y_arrs = np.array_split(Y,2)
 twoD_X = np.column_stack((X_arrs[0],X_arrs[1]))
 twoD_Y = np.column_stack((Y_arrs[0],Y_arrs[1]))
 
 
-# Pack X and Y into a single 3-dimensional array
 
+# Pack X and Y into a single 3-dimensional array
 data_pos = np.empty(twoD_X.shape + (2,))
 data_pos[:, :, 0] = twoD_X
 data_pos[:, :, 1] = twoD_Y
@@ -67,18 +70,18 @@ curve2.set_probabilities(Z2)
 #################
 # calculating responsibility
 ####### shape changes when probabilities are set for some reason. Put back getters to fix
-
 curve1.set_responsibilities((curve1.pi*curve1.probabilities)/(curve2.pi*curve2.probabilities+curve1.pi*curve1.probabilities))
 curve2.set_responsibilities((curve2.pi*curve2.probabilities)/(curve2.pi*curve2.probabilities+curve1.pi*curve1.probabilities))
 
 ###################
 #Plot scatter points
 ###plot###
-bx = benign.plot(kind='scatter',x = X_label, y = Y_label,color='red', label = 'malignant')
-malignant.plot(kind='scatter',x = X_label, y = Y_label, color='blue', ax=bx, label='benign')
 
-bx.set_xlabel(X_label)
-bx.set_ylabel(Y_label)
+bx = good.plot(kind='scatter',x = X_label, y = Y_label,color='red', label = 'good', s = 2)
+bad.plot(kind='scatter',x = X_label, y = Y_label, color='blue', ax=bx, label='bad', s =2)
+#
+# bx.set_xlabel(X_label)
+# bx.set_ylabel(Y_label)
 
 plt.plot(curve1.mu[0],curve1.mu[1],'gx')
 plt.plot(curve2.mu[0],curve2.mu[1],'gx')
@@ -89,29 +92,29 @@ a = plt.subplot()
 plt.scatter(X, Y, color = 'grey',s = 2)
 
 
-#need to figure out how to get the cov of just one at a time
-#todo: figure out what Z is and what should be passed in
-#cov1 = (curve1.covar(X, Y, Z1))
-#cov2 = curve2.covar(X, Y, Z2)'
+
 gcc.iterate(curve1,curve2,X,Y)
-# gcc.iterate(curve1,curve2,X,Y)
-# gcc.iterate(curve1,curve2,X,Y)
-# gcc.iterate(curve1,curve2,X,Y)
+gcc.iterate(curve1,curve2,X,Y)
+gcc.iterate(curve1,curve2,X,Y)
+gcc.iterate(curve1,curve2,X,Y)
 cov1 = curve1.sigma
 cov2 = curve2.sigma
+
 
 lambda1_, v1 = np.linalg.eig(cov1)
 lambda2_, v2 = np.linalg.eig(cov2)
 
 lambda1_ = np.sqrt(lambda1_)
 lambda2_ = np.sqrt(lambda2_)
-print(lambda1_, lambda2_)
-width1 = lambda1_[0] * 2
-height1 = lambda1_[1] * 2
-width2 = lambda2_[0] * 2
-height2 = lambda2_[1] * 2
+# print(lambda1_, lambda2_)
+# w, h = 2 * nstd * np.sqrt(vals)
+width1 = lambda1_[0] * 2 *3
+height1 = lambda1_[1] * 2*3
+width2 = lambda2_[0] * 2*3
+height2 = lambda2_[1] * 2*3
 angle1 = math.degrees(math.acos(v1[0, 0]))
 angle2 = math.degrees(math.acos(v2[0, 0]))
+
 
 e1 = Ellipse(curve1.mu, width1, height1, angle1)
 e2 = Ellipse(curve2.mu, width2, height2, angle2)
@@ -126,6 +129,6 @@ a.add_artist(e2)
 ################# MAXIMIZATION STEP ################################
 
 
-bx.plot(curve1.mu[0],curve1.mu[1],'co', markersize =15)
-bx.plot(curve2.mu[0],curve2.mu[1],'co', markersize =15)
+# bx.plot(curve1.mu[0],curve1.mu[1],'co', markersize =15)
+# bx.plot(curve2.mu[0],curve2.mu[1],'co', markersize =15)
 plt.show()
